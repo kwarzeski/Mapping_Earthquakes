f8d5ed08548a4f9d6e027a1b7ceb8cb30e511525
Kelsey Olsen <kwarzeski@gmail.com>
2022-05-21 16:02:10 -0400

    legend added

--- Earthquakes_past7days/static/js/logicStep4.js
+++ Earthquakes_past7days/static/js/logicStep4.js
@@ -89,7 +89,7 @@ function getRadius(magnitude) {
 L.geoJSON(data, {
     // We turn each feature into a circleMarker on the map.
     pointToLayer: function(feature, latlng) {
-        console.log(feature);
+        // console.log(feature);
         return L.circleMarker(latlng);
       },
     // We set the style for each circleMarker using our styleInfo function.
@@ -104,3 +104,32 @@ L.geoJSON(data, {
 earthquakes.addTo(map);
 
 });
+
+// Create a legend control object.
+let legend = L.control({
+  position: "bottomright"
+});
+
+// Then add all the details for the legend.
+legend.onAdd = function() {
+  let div = L.DomUtil.create("div", "info legend");
+  
+	const magnitudes = [0, 1, 2, 3, 4, 5];
+	const colors = [
+	  "#98ee00",
+	  "#d4ee00",
+	  "#eecc00",
+	  "#ee9c00",
+	  "#ea822c",
+	  "#ea2c2c"
+	];
+	
+	// Looping through our intervals to generate a label with a colored square for each interval.
+   for (var i = 0; i < magnitudes.length; i++) {
+     console.log(colors[i]);
+     div.innerHTML += "<i style='background: " + colors[i] + "'></i> " + magnitudes[i] + (magnitudes[i + 1] ? "&ndash;" + magnitudes[i + 1] + "<br>" : "+");
+  }
+   return div;
+ };
+
+ legend.addTo(map);
