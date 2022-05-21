215207dcc38389d5217fba875a397cd61297eb11
Kelsey Olsen <kwarzeski@gmail.com>
2022-05-21 15:35:35 -0400

    earthquake data from USGS

--- Earthquakes_past7days/static/js/logicStep1.js
+++ Earthquakes_past7days/static/js/logicStep1.js
@@ -20,27 +20,16 @@ let baseMaps = {
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-    center: [43.7, -79.3],
-    zoom: 11,
-    layers: [satelliteStreets]
+    center: [39.5, -98.5],
+    zoom: 3,
+    layers: [streets]
 })
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
-// Accessing the Toronto neighborhoods GeoJSON URL.
-let torontoHoods = "../../torontoNeighborhoods.json";
-
-// Grabbing our GeoJSON data.
-d3.json(torontoHoods).then(function(data) {
-    console.log(data);
+// Retrieve the earthquake GeoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
   // Creating a GeoJSON layer with the retrieved data.
-  L.geoJSON(data, {
-	  fill: "yellow",
-	  color: "blue",
-	  weight: 1,
-	  onEachFeature: function(feature, layer) {
-		  layer.bindPopup("<h3>Neighborhood: " + feature.properties.AREA_NAME + "</h3>");
-	  }
-  }).addTo(map);
+  L.geoJSON(data).addTo(map);
 });
