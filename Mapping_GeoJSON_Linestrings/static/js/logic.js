53cbc452485d74627c3c8098a4b3b4df5db21573
Kelsey Olsen <kwarzeski@gmail.com>
2022-05-21 14:51:41 -0400

    geoJSON Linestrings

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -1,5 +1,5 @@
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -14,7 +14,7 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 // Create a base layer that holds both maps.
 let baseMaps = {
-  Street: streets,
+  Light: light,
   Dark: dark
 };
 
@@ -22,22 +22,24 @@ let baseMaps = {
 let map = L.map('mapid', {
     center: [30, 30],
     zoom: 2,
-    layers: [streets]
+    layers: [dark]
 })
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
-// Accessing the airport GeoJSON URL
-let airportData = "../../majorAirports.json";
+// Accessing the Toronto airline routes GeoJSON URL.
+let torontoData = "../../torontoRoutes.json";
 
 // Grabbing our GeoJSON data.
-d3.json(airportData).then(function(data) {
+d3.json(torontoData).then(function(data) {
     console.log(data);
   // Creating a GeoJSON layer with the retrieved data.
   L.geoJSON(data, {
-    onEachFeature: function(feature, layer) {
-      layer.bindPopup("<h2>Airport Code: " + feature.properties.icao + "</h2> <hr /> <p><b>Airport Name: "+ feature.properties.name +"<b></p>");
-	}
+	  color: "#ffffa1",
+	  weight: 2,
+	  onEachFeature: function(feature, layer) {
+		  layer.bindPopup("<h3>Airline: " + feature.properties.airline + "</h3> <hr /> <b> Destination: "  + feature.properties.dst + "</b>");
+	  }
   }).addTo(map);
 });
